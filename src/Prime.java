07913ea01601164d12b1c9531bdc755c51220749
willc <wchokbengboune@ggc.edu>
2023-02-27 07:51:11 -0500

    initialized scanner

--- src/Prime.java
+++ src/Prime.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Prime {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-
+        System.out.println("Enter a number: ");
+        int number = in.nextInt();
+        for (int x = 2;x >= number -1; x++){
+            System.out.println("Sample output for value " + number + ":");
+            if (number % x != 0){
+                System.out.println(number + " is a prime number.");
+            }
+            else{
+                System.out.println(number + " is not a prime number.");
+            }
+        }
     }
 }

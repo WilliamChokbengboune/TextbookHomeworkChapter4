354593749dd6e476b571dd1dc1dbcbbf9b804fe6
willc <wchokbengboune@ggc.edu>
2023-03-13 07:54:33 -0400

    in.hasNextDouble statement was added to check for characters

--- src/Average.java
+++ src/Average.java
@@ -4,6 +4,17 @@ public class Average {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter grades: ");
+        int count = 0;
+        double sum = 0.0;
+        double temperature = 0.0;
+        while (in.hasNextDouble()){
+            temperature = in.nextDouble();
+            sum += temperature;
+            count++;
+        }
+        double average = sum/count;
+        System.out.println(temperature);
+        System.out.printf("Average student grade is %.2f.", average);
 
     }
 }
